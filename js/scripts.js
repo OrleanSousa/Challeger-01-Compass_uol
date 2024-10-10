5bba6c33ba578da9277f67a8aa7bbae9d5d772cb
Orlean Sousa <orleansilva60@hotmail.com>
2024-10-10 13:53:08 -0300

    feat: change in class calls

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ const resultMessageName = document.getElementById("resultMessageName")
 const emailForm = document.getElementById("emailForm")
 const subscribe = document.getElementById("subscribe")
 
-
 //valida email
 const validarEmail = (email)  =>{
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -21,33 +20,51 @@ const validaName = (name) =>{
 emailForm.addEventListener("submit", function(event){
     event.preventDefault()
 
-    const email = emailInput.value
-    const name = nameInput.value
-    const isValidName = validaName(name)
-    const isValid = validarEmail(email)
+    mensagemEmail()
+    mensagemName()
+    
+    
+})
+
+function mensagemEmail () {
 
     //mensagem para avisar se e-mail e valido ou não
-    if(isValid) {
-        localStorage.setItem("email", email)
+    const email = emailInput.value
+    const isValid = validarEmail(email)
+    if(isValid) {  
         resultMessage.textContent = "E-mail válido!"
         resultMessage.style.color = "green"
+        salveEmail()
     }else {
         resultMessage.textContent = "E-mail inválido!"
         resultMessage.style.color = "red"
     }
+}
+
+function mensagemName () {
+    const name = nameInput.value
+    const isValidName = validaName(name)
     //mensagem para avisar se nome esta valido
     if(isValidName) {
-        localStorage.setItem("name", name)
         resultMessageName.textContent = "Nome válido!"
         resultMessageName.style.color = "green"
+        salveName()
     } else {
         resultMessageName.textContent = "Nome inválido!"
         resultMessageName.style.color = "red"
     }
-
     
-})
+}
 
-
-
+// salve email no localStorage
+function salveEmail (){
+    if(mensagemEmail == true && mensagemName ==  true){
+        let salve =  localStorage.setItem("email", emailInput.value)
+        return salve
+    }
+}
+//salva nome no localStorage
+function salveName(){
+    localStorage.setItem("name", nameInput.value)
+}
 
