05c67caa402b1a739791507e78436c03c47e8dfc
Orlean Sousa <orleansilva60@hotmail.com>
2024-10-10 12:07:19 -0300

    feat: adding a new feature

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,20 @@ const nameInput = document.getElementById("name")
 const resultMessage = document.getElementById("resultMessage")
 const resultMessageName = document.getElementById("resultMessageName")
 const emailForm = document.getElementById("emailForm")
+const subscribe = document.getElementById("subscribe")
 
 
+//valida email
+const validarEmail = (email)  =>{
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+} // aqui ta ok ( o validador )
+
+//valida nome
+const validaName = (name) =>{
+    const regex = /^[A-Za-z]+(\s[A-Za-z]+)*$/
+    return regex.test(name)
+}
 
 emailForm.addEventListener("submit", function(event){
     event.preventDefault()
@@ -16,6 +28,7 @@ emailForm.addEventListener("submit", function(event){
 
     //mensagem para avisar se e-mail e valido ou não
     if(isValid) {
+        localStorage.setItem("email", email)
         resultMessage.textContent = "E-mail válido!"
         resultMessage.style.color = "green"
     }else {
@@ -24,25 +37,16 @@ emailForm.addEventListener("submit", function(event){
     }
     //mensagem para avisar se nome esta valido
     if(isValidName) {
+        localStorage.setItem("name", name)
         resultMessageName.textContent = "Nome válido!"
         resultMessageName.style.color = "green"
     } else {
         resultMessageName.textContent = "Nome inválido!"
         resultMessageName.style.color = "red"
     }
+
+    
 })
-
-
-//valida email
-const validarEmail = (email)  =>{
-    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(email);
-} // aqui ta ok ( o validador )
-
-const validaName = (name) =>{
-    const regex = /^[A-Za-z]+(\s[A-Za-z]+)*$/
-    return regex.test(name)
-}
 
 
 
